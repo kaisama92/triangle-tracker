89381854d55db4d0955ca9424aac7a70dd31aeae
Kai Clausen <kaiclausen123@gmail.com>
2023-01-12 14:49:24 -0800

    further add else if statements and create event handlers.
    Co-Authored by: Jannon Sielaff <jannon.sielaff@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
-let inputA = parseInt.document.getElementById("inputA");
-let inputB = parseInt.document.getElementById("inputB");
-let inputC = parseInt.document.getElementById("inputC");
-let output = document.querySelector("output")
+function handleTriangle(event) {
+  event.preventDefault();
+  let inputA = parseInt.document.getElementById("inputA");
+  let inputB = parseInt.document.getElementById("inputB");
+  let inputC = parseInt.document.getElementById("inputC");
+  let output = document.querySelector("output")
 
-function (triangleCalc) {
-  if (inputA === inputB && inputB === inputC) {
-    output = "equilateral"
-  } else if ((inputA ===  inputB && !inputC) || (inputA ===  inputC && !inputB) || (inputB === inputC && !inputA)) {
-    output = "isosceles"
-  } else if (inputA !== inputB && inputA !== inputC && inputB !== inputC) {
-    output = "scalene" 
+  function triangleCalc() {
+    if (inputA === inputB && inputB === inputC) {
+      output = "equilateral"
+    } else if ((inputA ===  inputB && !inputC) || (inputA ===  inputC && !inputB) || (inputB === inputC && !inputA)) {
+      output = "isosceles"
+    } else if (inputA !== inputB && inputA !== inputC && inputB !== inputC) {
+      output = "scalene" 
+    } else if (inputA + inputB <= inputC){
+      output = "not a triangle"
+    } else if (inputB + inputC <= inputA){
+      output = "not a triangle"
+    } else if (inputA + inputC <= inputB){
+      output = "not a triangle"
+    }
   }
+  document.getElementById("output").innerText = output;
 }
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("Triangles");
+  form.addEventListener("submit", handleTriangle);
+});
+
+
+
+
